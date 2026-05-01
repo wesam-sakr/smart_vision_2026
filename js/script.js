d52f12ecc671e85672d9fac526b0eb1fd9e66f81
wesam-sakr <wesamsakr801@gmail.com>
2026-05-01 11:46:20 +0300

    land

--- js/script.js
+++ js/script.js
@@ -63,95 +63,92 @@ document.addEventListener("DOMContentLoaded", () => {
   // تحديث كل ثانية
   setInterval(updateDateTime, 1000);
 
-  const stage = document.getElementById("canvas");
-  const todayKey = "canvas_last_seen";
-  const nowDate = new Date().toDateString();
+  // const stage = document.getElementById("canvas");
+  // const todayKey = "canvas_last_seen";
+  // const nowDate = new Date().toDateString();
 
-  // لو اتعرض النهارده قبل كده → اخفيه فورًا
-  if (localStorage.getItem(todayKey) === nowDate) {
-    stage.style.display = "none";
-  } else {
-    // سجّل إنه اتعرض النهارده
-    localStorage.setItem(todayKey, nowDate);
+  // if (localStorage.getItem(todayKey) === nowDate) {
+  //   stage.style.display = "none";
+  // } else {
+  //   localStorage.setItem(todayKey, nowDate);
 
-    stage.style.display = "block";
-    stage.width = window.innerWidth;
-    stage.height = window.innerHeight;
+  //   stage.style.display = "block";
+  //   stage.width = window.innerWidth;
+  //   stage.height = window.innerHeight;
 
-    let then, fpsInterval, startTime;
+  //   let then, fpsInterval, startTime;
 
-    const randItem = (i) => i[Math.floor(Math.random() * i.length)];
-    const rand = (i) => Math.floor(Math.random() * i);
-    const randBetween = (min, max) =>
-      Math.floor(Math.random() * (max - min + 1) + min);
+  //   const randItem = (i) => i[Math.floor(Math.random() * i.length)];
+  //   const rand = (i) => Math.floor(Math.random() * i);
+  //   const randBetween = (min, max) =>
+  //     Math.floor(Math.random() * (max - min + 1) + min);
 
-    const max = 30;
-    const ctx = stage.getContext("2d");
-    const w = stage.width;
-    const h = stage.height;
-    const pets = ["✨", "🌙"];
+  //   const max = 30;
+  //   const ctx = stage.getContext("2d");
+  //   const w = stage.width;
+  //   const h = stage.height;
+  //   const pets = ["✨", "🌙"];
 
-    const items = new Array(max).fill().map(() => {
-      return {
-        x: rand(w),
-        y: rand(h),
-        p: randItem(pets),
-        xs: -4 + Math.random() * 4 + 2,
-        ys: Math.random() * 1 + 1,
-        fs: randBetween(12, 46),
-      };
-    });
+  //   const items = new Array(max).fill().map(() => {
+  //     return {
+  //       x: rand(w),
+  //       y: rand(h),
+  //       p: randItem(pets),
+  //       xs: -4 + Math.random() * 4 + 2,
+  //       ys: Math.random() * 1 + 1,
+  //       fs: randBetween(12, 46),
+  //     };
+  //   });
 
-    function draw() {
-      ctx.clearRect(0, 0, w, h);
+  //   function draw() {
+  //     ctx.clearRect(0, 0, w, h);
 
-      items.forEach((p) => {
-        ctx.font = `${p.fs}px sans-serif`;
-        ctx.fillText(p.p, p.x, p.y);
-      });
+  //     items.forEach((p) => {
+  //       ctx.font = `${p.fs}px sans-serif`;
+  //       ctx.fillText(p.p, p.x, p.y);
+  //     });
 
-      requestAnimationFrame(draw);
+  //     requestAnimationFrame(draw);
 
-      let now = Date.now();
-      let elapsed = now - then;
+  //     let now = Date.now();
+  //     let elapsed = now - then;
 
-      if (elapsed > fpsInterval) {
-        then = now - (elapsed % fpsInterval);
-      }
+  //     if (elapsed > fpsInterval) {
+  //       then = now - (elapsed % fpsInterval);
+  //     }
 
-      move();
-    }
+  //     move();
+  //   }
 
-    function move() {
-      items.forEach((p) => {
-        p.x += p.xs;
-        p.y += p.ys;
+  //   function move() {
+  //     items.forEach((p) => {
+  //       p.x += p.xs;
+  //       p.y += p.ys;
 
-        if (p.x > w || p.y > h) {
-          p.x = rand(w);
-          p.y = -20;
-        }
-      });
-    }
+  //       if (p.x > w || p.y > h) {
+  //         p.x = rand(w);
+  //         p.y = -20;
+  //       }
+  //     });
+  //   }
 
-    function animate(fps) {
-      fpsInterval = 120 / fps;
-      then = Date.now();
-      startTime = then;
-      draw();
-    }
+  //   function animate(fps) {
+  //     fpsInterval = 120 / fps;
+  //     then = Date.now();
+  //     startTime = then;
+  //     draw();
+  //   }
 
-    animate(60);
+  //   animate(60);
 
-    // يخفي بعد دقيقة (60000 ملي ثانية)
-    setTimeout(() => {
-      stage.style.opacity = "0";
-      stage.style.pointerEvents = "none";
-      setTimeout(() => {
-        stage.style.display = "none";
-      }, 500);
-    }, 10000);
-  }
+  //   setTimeout(() => {
+  //     stage.style.opacity = "0";
+  //     stage.style.pointerEvents = "none";
+  //     setTimeout(() => {
+  //       stage.style.display = "none";
+  //     }, 500);
+  //   }, 10000);
+  // }
 
   // change 2nd word color
   const paragraph = document.querySelector(".logo-img span");
